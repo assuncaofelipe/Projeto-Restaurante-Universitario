50fdd766a4dabc8c7892fe104f8e764f79ce5541
Felipe V Assunção <felippevassuncao@gmail.com>
2022-06-23 02:12:09 -0400

    criação da ui e funcionalidade da SplashScreen criação da ui login criação da ui home

---
+++ app/src/main/java/com/icet/restaurante/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.icet.restaurante;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btn_login = findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
