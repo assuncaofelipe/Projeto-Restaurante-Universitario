2b3ef09fc0782016ceeb56d82b83cb2efdacc435
Felipe V Assunção <felippevassuncao@gmail.com>
2022-07-12 23:45:13 -0400

    melhorias de interface

--- app/src/main/java/com/icet/restaurante/FichasActivity.java
+++ app/src/main/java/com/icet/restaurante/FichasActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class FichasActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fichas);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back);
     }
 }
